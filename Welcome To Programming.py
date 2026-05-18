2942e076b8b1879936d68e3e044fc6742e39f767
Marvan <vermamarvan3@gmail.com>
2026-05-18 18:22:12 +0800

    main 1

---
+++ Welcome To Programming.py
@@ -0,0 +1,9 @@
+print ("Welcome To The World Of Programming")
+#print a number
+print(4)
+#next line after print
+print("hello Python\n")
+#pass multipule arguments
+print("hello", 5)
+#end argument of print st
+print("welcome to ",end="Python")
