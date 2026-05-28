ae211bb083c97f1556e4b997c711c4d8344d6716
Marvan Verma <vermamarvan3@gmail.com>
2026-05-28 20:59:07 +0800

    Create Store Birthdays.py

---
+++ Store Birthdays.py
@@ -0,0 +1,14 @@
+from calendar import JUNE
+from calendar import JULY
+from calendar import OCTOBER
+from calendar import DECEMBER
+m=JUNE, 10
+s=JUNE, 13
+p=JULY, 17
+i=OCTOBER, 31
+b=DECEMBER, 25
+print("Mark's birthday is on",m)
+print("Sarah's birthday is on",s)
+print("Paul's birthday is on",p)
+print("Ian's birthday is on",i)
+print("Ben's birthday is on",b)
