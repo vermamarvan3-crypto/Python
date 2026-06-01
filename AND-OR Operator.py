59a8f912372c7d85eefdce44f2d46ca9f27c73ba
Marvan Verma <vermamarvan3@gmail.com>
2026-06-01 18:20:17 +0800

    Create AND-OR Operator.py

---
+++ AND-OR Operator.py
@@ -0,0 +1,15 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+c=int(input("Enter a third number: "))
+if a and b and c:
+    print("All the numbers have boolean value of True")
+else:
+    print("At least one of the numbers has boolean value of False")
+
+d=int(input("Enter a number: "))
+e=int(input("Enter another number: "))
+f=int(input("Enter a third number: "))
+if d>0 or e>0 or f>0:
+    print("At least one of the numbers are greater than zero")
+else:
+    print("None of the numbers are greater than zero")
