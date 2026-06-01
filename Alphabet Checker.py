c8c95039aa977f74b7d915f1fc4d3b37ce4b79ea
Marvan Verma <vermamarvan3@gmail.com>
2026-06-01 19:10:24 +0800

    Create Alphabet Checker.py

---
+++ Alphabet Checker.py
@@ -0,0 +1,5 @@
+a=input("Enter A Letter Or A Number: ")
+if a=="a" or a=="b" or a=="c" or a=="d" or a=="e" or a=="f" or a=="g" or a=="h" or a=="i" or a=="j" or a=="k" or a=="l" or a=="m" or a=="n" or a=="o" or a=="p" or a=="q" or a=="r" or a=="s" or a=="t" or a=="u" or a=="v" or a=="w" or a=="x" or a=="y" or a=="z":
+    print(a,"is in the alphabet")
+else:
+    print(a,"is not in the alphabet")
