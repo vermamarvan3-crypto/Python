91ec1ebd03654b913d8d5be0a13e2f329d173f31
Marvan Verma <vermamarvan3@gmail.com>
2026-06-08 18:47:26 +0800

    Create Average Speed.py

---
+++ Average Speed.py
@@ -0,0 +1,16 @@
+a=10
+b=20
+c=30
+average=(a+b+c)/3
+if a<average:
+    print(a,"is less than the average")
+else:
+    print(a,"is greater than the average")
+    if b<average:
+        print(b,"is less than the average")
+    else:
+        print(b,"is greater than the average")
+        if c<average:
+            print(c,"is less than the average")
+        else:
+            print(c,"is greater than the average")
