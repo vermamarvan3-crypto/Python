fbc60c347f05d07f395032f38ed35464ce046d31
Marvan Verma <vermamarvan3@gmail.com>
2026-06-09 18:44:07 +0800

    Create Customize Your Ride.py

---
+++ Customize Your Ride.py
@@ -0,0 +1,13 @@
+ride=input("Choose your ride (Car/Bike): ")
+if ride=="Car" or ride=="car":
+    car1=input("Choose your car (Sedan/SUV): ")
+    if car1=="Sedan" or car1=="sedan":
+        print("You have chosen a Sedan car.")
+    elif car1=="SUV" or car1=="suv":
+        print("You have chosen an SUV car.")
+if ride=="Bike" or ride=="bike":
+        bike=input("Choose your bike (Sports/Normal): ")
+        if bike=="Sports" or bike=="sports":
+            print(" You hve chosen a Sports bike.")
+        elif bike=="Normal" or bike=="normal":
+            print("You have chosen a Normal bike.")
