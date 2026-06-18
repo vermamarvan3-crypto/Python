3fc2c8a45f78d53b7c111661b048f13932039255
Marvan Verma <vermamarvan3@gmail.com>
2026-06-18 18:22:51 +0800

    Create Folyd's Triangle.py

---
+++ Folyd's Triangle.py
@@ -0,0 +1,8 @@
+print("Half Pyramid Pattern")
+n=int(input("Enter The Number Of Rows: "))
+number=1
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(number,end=" ")
+        number=number+1
+    print()
