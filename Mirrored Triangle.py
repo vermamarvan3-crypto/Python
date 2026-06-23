6b1985227a21ac7862933544b03169b7f5230e1f
Marvan Verma <vermamarvan3@gmail.com>
2026-06-23 15:03:43 +0530

    Create Mirrored Triangle.py

---
+++ Mirrored Triangle.py
@@ -0,0 +1,19 @@
+rows = 4
+
+# Upper half
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+    for j in range(1, i + 1):
+        print(j, end="")
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    print()
+
+# Lower half
+for i in range(rows - 1, 0, -1):
+    print(" " * (rows - i), end="")
+    for j in range(1, i + 1):
+        print(j, end="")
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    print()
